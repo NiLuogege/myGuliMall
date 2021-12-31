36901daf137523426f7e6d636750403757492529
luoChen <hank.luo@xianghuanji.com>
2021-12-31 10:55:13 +0800

    服务注册都OK

--- gulimail-coupon/src/main/java/com/niluogege/gulimailcoupon/GulimailCouponApplication.java
+++ gulimail-coupon/src/main/java/com/niluogege/gulimailcoupon/GulimailCouponApplication.java
@@ -2,7 +2,9 @@ package com.niluogege.gulimailcoupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //启用服务发现
 @SpringBootApplication
 public class GulimailCouponApplication {
 
