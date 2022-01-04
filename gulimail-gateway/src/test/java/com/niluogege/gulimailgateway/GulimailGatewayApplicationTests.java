967b936958d72478e72de0c5627dfcdfaea91411
luoChen <hank.luo@xianghuanji.com>
2022-01-04 16:08:13 +0800

    添加网关

---
+++ gulimail-gateway/src/test/java/com/niluogege/gulimailgateway/GulimailGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niluogege.gulimailgateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
