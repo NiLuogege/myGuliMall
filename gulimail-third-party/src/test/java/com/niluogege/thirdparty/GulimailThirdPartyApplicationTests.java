72c587367ccca5ee6a23dff259db60d780d7af44
luoChen <hank.luo@xianghuanji.com>
2022-01-05 17:45:20 +0800

    新增 third-party 并引入 ali oss

---
+++ gulimail-third-party/src/test/java/com/niluogege/thirdparty/GulimailThirdPartyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niluogege.thirdparty;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailThirdPartyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
