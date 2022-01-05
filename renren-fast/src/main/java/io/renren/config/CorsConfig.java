2bfb6cd4464aefa7ebf8abd98705daf1191e2dba
luoChen <hank.luo@xianghuanji.com>
2022-01-05 15:40:46 +0800

    解决跨域

--- renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -12,15 +12,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .maxAge(3600);
-    }
-}
+/**
+ * 和gateway 中 冲突了 要注释掉
+ */
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            .allowedOrigins("*")
+//            .allowCredentials(true)
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .maxAge(3600);
+//    }
+//}
